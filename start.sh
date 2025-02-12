b2ad3ede6c201707436ee0c249a22a43350db33b
SURAJ PAWAR <121868743+Suraj7698@users.noreply.github.com>
2025-02-12 17:09:20 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+# Export proxy environment variables
+export HTTP_PROXY="http://your-proxy-server:port"
+export HTTPS_PROXY="http://your-proxy-server:port"
+export NO_PROXY="localhost,127.0.0.1"
+
+# Remaining script
+
+#!/bin/bash
 sudo apt update -y && sudo apt upgrade -y
 
 # Install essential packages
